8d914a05eb7f507cf03eef1d2594d5befe45d2da
Howar Li <howar.li@general-int.com>
2023-06-27 11:21:38 +0800

    add some optimization

--- task1.py
+++ task1.py
@@ -1,37 +1,51 @@
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
+from multiprocessing import cpu_count
 import copy
 
 directions = [(-1,0), (1,0), (0,1), (0,-1)]
 
-def args_zip(x : int, y : int, cnt : int, n, visited, deepth):
-    return ((x), (y), (cnt), (n), copy.deepcopy(visited), (deepth))
+def args_zip(x : int, y : int, cnt : int, n, visited, deepth, path_tag):
+    return (x, y, cnt, n, copy.deepcopy(visited), deepth, path_tag)
 
 def walk_ziped_args(args):
-    return walk(args[0], args[1], args[2], args[3], args[4], args[5])
+    return walk(args[0], args[1], args[2], args[3], args[4], args[5], args[6])
 
-def walk(x : int, y : int, cnt : int, n, visited, deepth, args_pool = None):
+def walk(x : int, y : int, cnt : int, n, visited, deepth, path_tag, args_pool = None):
     # print(x, y, cnt)
+    if y == 1:
+        if(path_tag == 2):
+            return 0
+        path_tag = 1
     if cnt < 1:
-        return 1
-    if (args_pool is not None) and deepth > 7 and (cnt > 3):
-        args_pool.append(args_zip(x, y, cnt, n, copy.deepcopy(visited), deepth))
+        return 2 - (path_tag != 0)
+    if (args_pool is not None) and (deepth > 7):
+        args_pool.append(args_zip(x, y, cnt, n, copy.deepcopy(visited), deepth, path_tag))
         return 0
+    
     visited[x][y] = True
     ans = 0
     for dire in directions:
         x2 = x + dire[0]
         y2 = y + dire[1]
-        if (x2 <= n) and (0 <= x2) and (y2 <= n) and (0 <= y2) and (not visited[x2][y2]):
-            ans += walk(x2, y2, cnt-1, n, visited, deepth+1, args_pool)
+        if (x2 <= n+1) and (y2 <= n+1) and (1 <= y2) and (not visited[x2][y2]):
+            if (1 <= x2):
+                ans += walk(x2, y2, cnt-1, n, visited, deepth+1, copy.deepcopy(path_tag), args_pool)
+            elif (0 == x2) and (path_tag != 1):
+                ans += walk(x2, y2, cnt-1, n, visited, deepth+1, 2, args_pool)
+
     visited[x][y] = False
     return ans
 
 def solve(n):
-    visited = [[False for i in range(n+1)] for i in range(n+1)]
-    visited[0][0] = True
-    with ProcessPoolExecutor(max_workers=60) as executor:
+    if n == 1:
+        return 1
+    visited = [[False for i in range(n+2)] for i in range(n+2)]
+    visited[1][1] = True
+    visited[0][2] = True
+    visited[1][2] = True
+    with ProcessPoolExecutor(max_workers = cpu_count()) as executor:
         args_pool = []
-        ans = walk(0, 1, n - 1, n, visited, 1, args_pool)
+        ans = walk(2, 2, n - 2, n, visited, 2, 0, (args_pool if n > 10 else None))
         print(f"len(args_pool): {len(args_pool)}")
         tasks_list = []
         for args in args_pool:
@@ -44,7 +58,7 @@ def solve(n):
 
 def main():
     n = int(input())
-    # n = 24
+    # n = 22
     ans = solve(n)
     print(n, ans)
     
@@ -77,19 +91,4 @@ if __name__ == "__main__":
 23 600281932
 24 1552096361
 25 4017128206
-26 10401997092
-27 26957667445
-28 69892976538
-29 181340757857
-30 470680630478
-31 1222433229262
-32 3175981845982
-33 8255898715518
-34 21467989716002
-35 55850067698545
-36 145339886752867
-37 378379746027286
-38 985346905294729
-39 2566922857037007
-40 6688754646791611
 '''
